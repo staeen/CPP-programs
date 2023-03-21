f6a2134f68f55408e79f52232dacd55ddfe01706
staeen <stan.dorjey2014@gmail.com>
2023-03-21 21:40:02 +0530

    Test Update

--- test.cpp
+++ test.cpp
@@ -4,41 +4,14 @@ Pointers Exercise
 #include <iostream>
 using namespace std;
 
-void print(int *p)
-{
-
-    cout << *p << endl;
-}
-void update(int *p)
-{
-
-    //    p = p + 1;
-    //    cout << " Inside update " << p << endl;
-    *p = *p + 1;
-}
-
-int getsum(int *arr, int n)
-{
-    cout << " Size : " << sizeof(arr) << endl;
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += i[arr];
-    }
-    return sum;
-}
-
 int main()
 {
-    int arr[6] = {1, 2, 3, 4, 5, 8};
-    int value = 5;
-    int *p = &value;
-    // print(p);
-    cout << " Before " << *p << endl;
-    update(p);
-    cout << " After " << *p << endl;
-    cout << " For array sum " << endl;
-    int z = getsum(arr + 3, 3);
-    cout << " Sum is " << z << endl;
+    int i = 5;
+    int *p1 = &i;
+    int **p2 = &p1;
+    int ***p3 = &p2;
+    cout << "Mem Loc : " << p1 << " Value : " << *p1 << endl;
+    cout << "Mem Loc : " << p2 << " Value : " << *p2 << endl;
+    cout << "Mem Loc : " << p3 << " Value : " << *p3 << endl;
     return 0;
 }
