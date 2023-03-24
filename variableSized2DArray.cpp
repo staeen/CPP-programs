e3f4998ff8f6b2247519950bc6e5e268dd356816
staeen <stan.dorjey2014@gmail.com>
2023-03-24 11:05:45 +0530

    Made on 24 march 2023

--- variableSized2DArray.cpp
+++ variableSized2DArray.cpp
@@ -1,18 +1,8 @@
 /*
-Pointers Exercise
+Variable sized 2D array
 */
 #include <iostream>
 using namespace std;
-
-int getSum(int *arr, int n)
-{
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    return sum;
-}
 
 int main()
 {
