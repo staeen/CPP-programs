f3306b6ea015ff02d5a3dd05379efc8484182ec7
staeen <stan.dorjey2014@gmail.com>
2023-03-24 15:51:00 +0530

    Made on 24 march 2023

--- recurFact.cpp
+++ recurFact.cpp
@@ -5,12 +5,12 @@ Factorial using recursion
 #include <iostream>
 using namespace std;
 
-int power(int n)
+int fact(int n)
 {
     // base case
     if (n == 0)
         return 1;
-    return 2 * power(n - 1);
+    return n * fact(n - 1);
 }
 
 int main()
@@ -18,7 +18,7 @@ int main()
     int n;
     cout << "Enter n " << endl;
     cin >> n;
-    int ans = power(n);
-    cout << "Power is " << ans << endl;
+    int ans = fact(n);
+    cout << "Fatorial is " << ans << endl;
     return 0;
 }
